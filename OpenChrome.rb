b2be27d18c6dad62618f4efa2046a97bed1e0fe0
Jose Alberto Tolentino Hdz <32423935+ToleHdz@users.noreply.github.com>
2018-02-27 14:52:50 -0600

    accediendo a las propiedades de HTML

--- OpenChrome.rb
+++ OpenChrome.rb
@@ -5,6 +5,21 @@ Selenium::WebDriver::Chrome.driver_path = '/Applications/Google Chrome.app/Conte
 
 Navegador = Watir::Browser.new :chrome
 
-Navegador.goto 'http://www.google.com.mx'
-sleep(3)
-Navegador.close
+Navegador.goto 'file:///Users/visita/Documents/GitHub/Node.js/html/MiPagina.html'
+
+#escribimos sobre nuestras propiedades
+#Navegador.text_field(:id => 'nombre').set 'Michii - Sama'
+
+#accedemos a los gets del elemento
+#puts Navegador.text_field(:id => 'nombre').value
+
+MisP = Navegador.ps
+
+MisP.each do |p|
+   puts "Tengo una etiqueta con el id #{p.id}"
+   puts "Valor de la etiqueta: #{p.text}"
+   puts ' '
+end
+
+puts "id: #{MisP[5].id} & text: #{MisP[5].text}"
+sleep()
